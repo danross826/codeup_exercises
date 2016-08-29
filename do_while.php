71fe18a12a7bb8a1437fd842642f9ba078f0739a
Dan Ross <danross826@gmail.com>
2016-08-29 16:57:03 -0500

    done with the exercises for 08/29/16

---
+++ do_while.php
@@ -0,0 +1,69 @@
+<?php
+
+$test=0;
+
+do{
+	echo $test . PHP_EOL;
+	$test=$test+2;
+} while ( $test<= 100);
+
+$testTwo=100;
+
+do{
+	echo $testTwo . PHP_EOL;
+	$testTwo=$testTwo-10;
+} while ( $testTwo>= 0);
+
+$a=2;
+
+do{
+	echo $a . PHP_EOL;
+	$a=$a*2;
+} while ( $a< 1000000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
